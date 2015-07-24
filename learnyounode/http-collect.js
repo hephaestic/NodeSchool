675d6c032eca15110f79cb4e9c284a23a1d05f1c
Kevin Seng <hephaestic@gmail.com>
2015-07-24 01:11:28 -0700

    completed http-collect with bl

--- learnyounode/http-collect.js
+++ learnyounode/http-collect.js
@@ -1,38 +1,12 @@
-// var http = require('http');
-
-// http.get(process.argv[2], function(response) {
-//     var len = 0;
-//     var str = '';
-//     response.setEncoding("utf8");
-//     response.on("data", function(data) {
-//         len += data.length;
-//         str += data;
-//     });
-//     response.on("end", function(end) {
-//         console.log(len);
-//         console.log(str);
-//     });
-//     response.on("error", console.error);
-// });
-
 var url = process.argv[2];
 
 var http = require('http');
+var bl = require('bl');
 
 http.get(url, function (response){
-	//console.log("callback");
-	var numData = 0;
-	var allData = "";
-	response.setEncoding("utf8");
-	response.on("data", function (data){
-	//console.log("get");
-		numData += data.length;
-		allData += data;
-	});
-	response.on("end", function (end){
-	//console.log("end");
-		console.log(numData);
-		console.log(allData);
-	});
-	response.on("error", console.error);
+	response.pipe(bl(function (err, data){
+		if(err) return console.log(err);
+		console.log(data.length);
+		console.log(data.toString());
+	}));
 })
